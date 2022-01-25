d3a5151442024223fcca2ddc8b5f3721a31e3242
JeonJongYook <jggen0121@naver.com>
2022-01-25 22:12:49 +0900

    Login페이지 생성

---
+++ src/components/LogIn.vue
@@ -0,0 +1,11 @@
+<template src="./src/components/Login.vue">
+</template>
+
+<script>
+
+export default {
+  name: "login"
+}
+</script>
+<style src="./src/components/css/style.css" scoped>
+</style>
