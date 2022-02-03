8cc47a4e7d4290fef0b77f4bc70487af2a9341db
JeonJongYook <jggen0121@naver.com>
2022-02-03 19:36:00 +0900

    App.vue파일 수정

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <signup msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-<<<<<<< HEAD
-import HelloWorld from './components/HelloWorld.vue'
-=======
-import HelloWorld from './components/common/SignUp.vue'
->>>>>>> 1968d7ef8bba78886d6719ad3bfe6b930ff658e2
+import login from 'src/components/common/LogIn.vue'
+import signup from 'src/components/common/SignUp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    signup,
+    login
   }
 }
 </script>
